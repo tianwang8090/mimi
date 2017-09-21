9def00b204843a881f712a4efbf361c697416e9f
tianwang <tianwang8090@163.com>
2017-09-21 23:11:03 +0800

    add write page

--- pages/write/write.js
+++ pages/write/write.js
@@ -4,70 +4,91 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    content: "",
+    contentImgSrc: "",
+    textareaOptions: {
+      autofocus: true,
+      focus: true,
+      placeholder: "写下你的MiMi..."
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
+  },
+  textareaWrapTapHandle() {
+    let obj = {},
+      s = "textareaOptions.focus";
+    obj[s] = true;
+    this.setData(obj);
   },
   /**
-   * 点击新建按钮
+   * 添加图片
    */
-  writeTapHandle(e) {
-    wx.navigateTo({
-      url: '../write/write'
+  chooseImgHandle(e) {
+    let _this = this;
+    wx.chooseImage({
+      count: 1,	// 默认为9
+      sizeType: ['original', 'compressed'],	// 指定原图或者压缩图
+      sourceType: ['album', 'camera'],	// 指定图片来源
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths;
+        _this.setData({
+          contentImgSrc: tempFilePaths[0]
+        });
+      }
     })
   }
 })
